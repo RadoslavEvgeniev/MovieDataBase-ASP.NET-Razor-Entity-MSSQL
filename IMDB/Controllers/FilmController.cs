e5118003a9c62f212202d8fc605d423ca505e5ea
Radoslav <r.evgeniev.ivanov@gmail.com>
2017-08-28 13:29:39 +0300

    MovieDb

---
+++ IMDB/Controllers/FilmController.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using IMDB.Models;
+
+namespace IMDB.Controllers
+{
+    [ValidateInput(false)]
+    public class FilmController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            using (var db = new IMDBDbContext())
+            {
+                var films = db.Films.ToList();
+                return View(films);
+            }
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Film film)
+        {
+            using (var db = new IMDBDbContext())
+            {
+                db.Films.Add(film);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int? id)
+        {
+            using (var db = new IMDBDbContext())
+            {
+                var film = db.Films.Find(id);
+                return View("Edit", film);
+            }
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int? id, Film filmModel)
+        {
+            using (var db = new IMDBDbContext())
+            {
+                var film = db.Films.Find(id);
+                film.Id = filmModel.Id;
+                film.Name = filmModel.Name;
+                film.Genre = filmModel.Genre;
+                film.Director = filmModel.Director;
+                film.Year = filmModel.Year;
+                
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public ActionResult Delete(int? id)
+        {
+            using (var db = new IMDBDbContext())
+            {
+                var film = db.Films.Find(id);
+                return View("Delete", film);
+            }
+        }
+
+        [HttpPost]
+        [Route("delete/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int? id, Film filmModel)
+        {
+            using (var db = new IMDBDbContext())
+            {
+                var film = db.Films.Find(id);
+                db.Films.Remove(film);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ IMDB/Models/Film.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace IMDB.Models
+{
+    public class Film
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public String Name { get; set; }
+
+        [Required]
+        public String Genre { get; set; }
+
+        [Required]
+        public String Director { get; set; }
+
+        [Required]
+        public String Year { get; set; }
+    }
+}
